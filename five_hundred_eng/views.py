39a9b84d02b86e1e5d59322b1d149fd3ee26c4f2
dextto <zd.gary@gmail.com>
2019-12-27 17:30:01 +0900

    BASE_DIR 이용해서 파일 경로 설정

--- five_hundred_eng/views.py
+++ five_hundred_eng/views.py
@@ -4,16 +4,18 @@ import os
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 class DbVersionView(APIView):
     def get(self, request):
-        with open('./five_hundred_eng/db_version') as f:
+        with open(f'{BASE_DIR}./five_hundred_eng/db_version') as f:
             db_version = f.readline()
         return Response(data=db_version)
 
 
 class ContentsView(APIView):
     def get(self, request):
-        with open('./five_hundred_eng/content.json', encoding='utf-8') as f:
+        with open(f'{BASE_DIR}./five_hundred_eng/content.json', encoding='utf-8') as f:
             contents = f.read()
         return Response(data=json.loads(contents))
