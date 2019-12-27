f2af75f229493810935f10034be41cffd5342664
dextto <zd.gary@gmail.com>
2019-12-27 17:31:05 +0900

    오타 수정

--- five_hundred_eng/views.py
+++ five_hundred_eng/views.py
@@ -9,13 +9,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class DbVersionView(APIView):
     def get(self, request):
-        with open(f'{BASE_DIR}./five_hundred_eng/db_version') as f:
+        with open(f'{BASE_DIR}/five_hundred_eng/db_version') as f:
             db_version = f.readline()
         return Response(data=db_version)
 
 
 class ContentsView(APIView):
     def get(self, request):
-        with open(f'{BASE_DIR}./five_hundred_eng/content.json', encoding='utf-8') as f:
+        with open(f'{BASE_DIR}/five_hundred_eng/content.json', encoding='utf-8') as f:
             contents = f.read()
         return Response(data=json.loads(contents))
